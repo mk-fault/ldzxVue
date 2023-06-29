525c6f5e0b8e69b3388637de3dc0f6eec88c4a3f
mk-fault <945495541@qq.com>
2023-06-29 14:02:10 +0800

    0629再修复token过期

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,27 +1,26 @@
 // axios基础的封装
 import axios from "axios";
-import { ElMessage } from 'element-plus'
-import 'element-plus/theme-chalk/el-message.css'
+import { ElMessage } from "element-plus";
+import "element-plus/theme-chalk/el-message.css";
 import { useUserStore } from "@/stores/user";
 import router from "@/router";
 
 const httpInstance = axios.create({
   baseURL: "http://127.0.0.1:8000/api",
-//   baseURL: "http://192.168.58.6:8000/api",
+  //   baseURL: "http://192.168.58.6:8000/api",
 
   timeout: 5000,
 });
 
-const userStore = useUserStore();
-
 // axios请求拦截器
 httpInstance.interceptors.request.use(
   (config) => {
-    // 从pinia中获取token 
+    // 从pinia中获取token
+    const userStore = useUserStore();
     // 拼接token
-    const token = userStore.userInfo.access
+    const token = userStore.userInfo.access;
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+      config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
@@ -36,12 +35,14 @@ httpInstance.interceptors.response.use(
     // ElMessage.error(e.response.data.msg);
     // 401错误处理
     if (e.response.status === 401) {
-        // 清除token
-        userStore.clearUserInfo()
-      router.push('/teacher/login')
+      // 清除token
+      const userStore = useUserStore();
+      userStore.clearUserInfo();
+      ElMessage.error("登录已过期，请重新登录");
+      router.push("/teacher/login");
     }
     // return Promise.reject(e);
-    return e.response
+    return e.response;
   }
 );
 
